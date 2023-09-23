1100516c109207970572d94f191c4f9b29a57962
Ryan Mercadante <ryan.a.mercadante@gmail.com>
2023-09-23 16:09:46 -0400

    add catch command

--- command_catch.go
+++ command_catch.go
@@ -11,22 +11,39 @@ func commandCatch(cfg *config, args ...string) error {
 		return errors.New("must provide a pokemon to catch")
 	}
 
-	pokemonName := args[0]
-	fmt.Printf("Throwing a Pokeball at %s...\n", pokemonName)
+	if cfg.currentLocation == nil {
+		return errors.New("there are no pokemon to catch in this location area")
+	}
 
-	pokemon, err := cfg.pokeapiClient.GetPokemonInfo(pokemonName)
+	pokemonName := args[0]
+	locationAreaName := *cfg.currentLocation
+
+	resp, err := cfg.pokeapiClient.GetLocationArea(locationAreaName)
 	if err != nil {
 		return err
 	}
 
-	const threshold = 50
-	randNum := rand.Intn(pokemon.BaseExperience)
-	if randNum > threshold {
-		return fmt.Errorf("failed to catch %s", pokemonName)
+	for _, pokemon := range resp.PokemonEncounters {
+		if pokemon.Pokemon.Name == pokemonName {
+			fmt.Printf("Throwing a Pokeball at %s...\n", pokemonName)
+
+			pokemon, err := cfg.pokeapiClient.GetPokemonInfo(pokemonName)
+			if err != nil {
+				return err
+			}
+
+			const threshold = 50
+			randNum := rand.Intn(pokemon.BaseExperience)
+			if randNum > threshold {
+				return fmt.Errorf("failed to catch %s", pokemonName)
+			}
+
+			fmt.Printf("%s was caught!\n", pokemonName)
+			cfg.caughtPokemon[pokemonName] = pokemon
+
+			return nil
+		}
 	}
 
-	fmt.Printf("%s was caught!\n", pokemonName)
-	cfg.caughtPokemon[pokemonName] = pokemon
-
-	return nil
+	return errors.New("can only catch pokemon in your location area")
 }
--- command_pokedex.go
+++ command_pokedex.go
@@ -3,9 +3,10 @@ package main
 import "fmt"
 
 func commandPokedex(cfg *config, _ ...string) error {
-	fmt.Println("Your Pokedex:")
+	fmt.Println("\nYour Pokedex:")
 	for k := range cfg.caughtPokemon {
 		fmt.Printf(" - %s\n", k)
 	}
+	fmt.Println()
 	return nil
 }
